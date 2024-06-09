70ef035d22ffa373b1600a260268ae54a91fbf42
Nitolez <antonioglezt98@gmail.com>
2024-06-09 17:41:54 +0200

    Log in and register applied

--- js/script.js
+++ js/script.js
@@ -218,7 +218,7 @@ document.addEventListener('DOMContentLoaded', () => {
         fechaAntiguo.innerText = 'OLDEST: ' + element.oldest_published_date;
         fechaNuevo.innerText = 'NEWEST: ' + element.newest_published_date;
         actualizacion.innerText = 'UPDATED: ' + element.updated;
-        linkLista.innerText = 'VER MÁS ->';
+        linkLista.innerText = 'SEE MORE ->';
         favorito.innerHTML = '<img src="../imgs/corazonVacio.png" width="15px" heigh="15px">'
         favorito.id = 'btnFavorito'
 
@@ -341,5 +341,100 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Inicio
     renderizar(); // Mostramos la primera página nada más que carge la página
+
+
+
+
+
+
+    //LOG IN
+
+    const signInUser = (email, password) => {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                // Signed in
+                let user = userCredential.user;
+                console.log(`se ha logado ${user.email} ID:${user.uid}`)
+                alert(`se ha logado ${user.email} ID:${user.uid}`)
+                console.log("USER", user);
+            })
+            .catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
+            });
+    }
+
+    const signOut = () => {
+        let user = firebase.auth().currentUser;
+
+        firebase.auth().signOut().then(() => {
+            console.log("Sale del sistema: " + user.email)
+        }).catch((error) => {
+            console.log("hubo un error: " + error);
+        });
+    }
+
+
+    document.getElementById("form2").addEventListener("submit", function (event) {
+        event.preventDefault();
+        let email = event.target.elements.email2.value;
+        let pass = event.target.elements.pass3.value;
+        signInUser(email, pass)
+    })
+    document.getElementById("salir").addEventListener("click", signOut);
+
+
+    // Listener de usuario en el sistema
+    // Controlar usuario logado
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log(`Está en el sistema:${user.email} ${user.uid}`);
+            document.getElementById("message").innerText = `You are logged as: ${user.email}`;
+        } else {
+            console.log("no hay usuarios en el sistema");
+            document.getElementById("message").innerText = `Log in or sign up`;
+        }
+    });
+
+
+
+
+    //SIGN UP
+
+    const signUpUser = (email, password) => {
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then((userCredential) => {
+            // Signed in
+            let user = userCredential.user;
+            console.log(`se ha registrado ${user.email} ID:${user.uid}`)
+            alert(`se ha registrado ${user.email} ID:${user.uid}`)
+            // ...
+            // Saves user in firestore
+            createUser({
+              id: user.uid,
+              email: user.email,
+              message: "hola!"
+            });
+      
+          })
+          .catch((error) => {
+            console.log("Error en el sistema" + error.message, "Error: " + error.code);
+          });
+      };
+      
+      
+      document.getElementById("form1").addEventListener("submit", function (event) {
+        event.preventDefault();
+        let email = event.target.elements.email.value;
+        let pass = event.target.elements.pass.value;
+        let pass2 = event.target.elements.pass2.value;
+      
+        pass === pass2 ? signUpUser(email, pass) : alert("error password");
+      })
+
 
 })
